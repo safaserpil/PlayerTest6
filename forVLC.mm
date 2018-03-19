0131ab102d8177b5e908e7ab11195e651aa082d1
Serpil SAFA <argemultitek@gmail.com>
2018-03-19 16:54:17 +0300

    test 2

---
+++ forVLC.mm
@@ -0,0 +1,13 @@
+//
+//  forVLC.mm
+//  PlayerTest7
+//
+//  Created by Adnan-iMac on 19.03.2018.
+//  Copyright © 2018 Serpil. All rights reserved.
+//
+
+#ifndef forVLC_h
+#define forVLC_h
+
+
+#endif /* forVLC_h */
